49d87851869ff5c3dae57d5f3f06bdb8c359d264
Cindy1229 <leticiashi3@gmail.com>
2020-06-10 12:51:24 -0400

    Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var canvas=document.getElementById('canvas');
+var ctx=canvas.getContext('2d');
+
+//load images
+var bird=new Image();
+var bg=new Image();
+var fg=new Image();
+var pipeNorth=new Image();
+var pipeSouth=new Image();
+
+
+bird.src="images/bird.png";
+bg.src="images/bg.png";
+fg.src="images/fg.png";
+pipeNorth.src="images/pipeNorth.png";
+pipeSouth.src="images/pipeSouth.png";
